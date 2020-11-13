ebcf3354c75ac1bfe1c04cb5f3e1f32e32603c1e
Dave Connors <daveconnors3@gmail.com>
2020-11-13 17:23:27 -0600

    incomplete eod 11/12

--- macros/generate_stage.sql
+++ macros/generate_stage.sql
@@ -4,14 +4,12 @@
 
 {%- set columns = adapter.get_columns_in_relation(source_relation) -%}
 
-{%- set id = [] -%}
 {%- set numeric = [] -%}
 {%- set datetime = [] -%}
 {%- set bool = [] -%}
 {%- set string = [] -%}
 
 
-{% for column in columns if column.column.lower() == 'id' %}{% do id.append(column) %}{%endfor%}
 {% for column in columns if column.is_numeric() or column.is_number() %}{% do numeric.append(column) %}{%endfor%}
 {% for column in columns if column.dtype.startswith('DATE') or column.dtype.startswith('TIME') %}{% do datetime.append(column) %}{%endfor%}
 {% for column in columns if column.dtype == 'BOOLEAN' %}{% do bool.append(column) %}{%endfor%}
@@ -33,19 +31,25 @@ renamed as (
     select
 
         -- ids
-        {%- for column in id %}
-        {{ column | lower }}{{","}}
-        {%- endfor %}
-        {%- for column in id %}
-        {{ column | lower }}{{","}}
-        {%- endfor %}
-        {%- for column in id %}
-        {{ column | lower }}{{","}}
-        {%- endfor %}
-        {%- for column in id %}
-        {{ column | lower }}{{","}}
+        {%- for column in numeric -%}
+        {{ column.column | lower }}
+        {% if not loop.last %},{% endif%}
         {%- endfor %}
 
+        {%- for column in datetime -%}
+        {{ column.column | lower }}
+        {% if not loop.last %},{% endif%}
+        {%- endfor %}
+
+        {%- for column in bool -%}
+        {{ column.column | lower }}
+        {% if not loop.last %},{% endif%}
+        {%- endfor %}
+
+        {%- for column in string -%}
+        {{ column.column | lower }}
+        {% if not loop.last %},{% endif%}
+        {%- endfor %}
 
     from source
 )
@@ -55,7 +59,7 @@ select * from renamed
 
 {% if execute %}
 
-{{ log(len(string), info=True) }}
+{{ log(base_model_sql, info=True) }}
 {# {% do return(column_names | lower ) %} #}
 {# {% do return(column_types) %} #}
 
